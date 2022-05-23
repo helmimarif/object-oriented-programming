747e8274205477ec2bbb069382c2fbb1bf78b07a
Ayse <106028948+helmimarif@users.noreply.github.com>
2022-05-23 10:38:30 +0700

    Class and Object

---
+++ 02. Tes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ayse
+ */
+public class Tes {
+    int x = 5;
+    
+    public static void main(String[] args) {
+        Tes myObj = new Tes();
+        System.out.println(myObj.x);
+    }
+}
