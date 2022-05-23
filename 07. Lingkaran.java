66e381dcbc57d698c32b4b7b36c4b65caf413723
Ayse <106028948+helmimarif@users.noreply.github.com>
2022-05-23 10:40:04 +0700

    Overriding

---
+++ 07. Lingkaran.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+/**
+ *
+ * @author Ayse
+ */
+public class Lingkaran extends BangunDatar{
+    
+    // jari-jari lingkaran
+    float r;
+    
+    @Override
+    float luas(){
+        float luas = (float) (Math.PI * r * r);
+        System.out.println("Luas lingkaran: " + luas);
+        return luas;
+    }
+    
+    @Override
+    float keliling(){
+        float keliling = (float) (2 * Math.PI * r);
+        System.out.println("Keliling Lingkaran: " + keliling);
+        return keliling;
+    }
+    
+}
